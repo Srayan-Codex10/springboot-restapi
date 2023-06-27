f07da153090070fbc0099f33d2e83f0ec7285628
Srayan-Codex10 <sroyon.ray@gmail.com>
2023-06-27 20:26:09 +0530

    code cleanup

--- src/main/java/com/restapi/app/TestApplication.java
+++ src/main/java/com/restapi/app/TestApplication.java
@@ -1,23 +1,15 @@
 package com.restapi.app;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
-import com.restapi.config.RestAppConfig;
 
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 @Slf4j
 public class TestApplication {
-
-	@Autowired
-	RestAppConfig appConfig;
-	
-	@Post
 
 	public static void main(String[] args) {
 		log.info("Starting Application V1");
